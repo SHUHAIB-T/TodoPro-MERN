0bf43a432242f63b0000836c022b615133e56715
Shuhaib T U <shuhaibtu79@gmail.com>
2024-04-11 18:50:05 +0530

    feat: db connection and protect middleware added

---
+++ server/src/model/userModel.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+export interface IUser {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+export default User;
